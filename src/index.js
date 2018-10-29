df9c9259ca36c8e0e35719a6a8e72c063abd3c41
Gingertonic <bethmschofield@gmail.com>
2018-10-29 20:05:53 +0000

    remove redux devtools

--- src/index.js
+++ src/index.js
@@ -9,12 +9,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
 import combineReducers from './reducers/root'
 
-const store = createStore(combineReducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+const store = createStore(combineReducers, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
